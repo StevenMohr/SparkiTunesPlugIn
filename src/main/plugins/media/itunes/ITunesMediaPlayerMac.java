5d7d28a6b5597d9e20c6de0916a3bccaf7c1e7ab
Steven Mohr <steven@stevenmohr.de>
2011-11-20 20:09:35 +0100

    implemented test and corresponding functionality

--- src/main/plugins/media/itunes/ITunesMediaPlayerMac.java
+++ src/main/plugins/media/itunes/ITunesMediaPlayerMac.java
@@ -3,19 +3,42 @@
  */
 package plugins.media.itunes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import plugins.media.MediaPlayer;
 import plugins.media.SongInfo;
 
 /**
- * @author steven
- *
+ * @author Steven Mohr
+ * 
  */
-public class ITunesMediaPlayerMac implements MediaPlayer{
+public class ITunesMediaPlayerMac implements MediaPlayer {
 
 	@Override
 	public SongInfo getCurrentPlayingSong() {
-		// TODO Auto-generated method stub
-		return null;
+		Runtime rt = Runtime.getRuntime();
+		SongInfo songInfo = new SongInfo();
+		try {
+			String[] args = { "osascript", "-e",
+			"tell app \"iTunes\" to artist of current track as string" };
+			Process proc = rt.exec(args);
+			InputStream in = proc.getInputStream();
+			String artist = new BufferedReader(new InputStreamReader(in)).readLine();
+			
+			String[] args2 = { "osascript", "-e",
+			"tell app \"iTunes\" to name of current track as string" };
+			proc = rt.exec(args2);
+			in = proc.getInputStream();
+			String title = new BufferedReader(new InputStreamReader(in)).readLine();
+			
+			songInfo.setInterpret(artist);
+			songInfo.setTitle(title);
+		} catch (IOException e) {
+			return null;
+		}
+		return songInfo;
 	}
-
 }
